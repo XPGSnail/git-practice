eadfbc6c03ce88264209725931484aaaeb0bb34e
pan da guo <pandaguo@pptv.com>
2017-12-05 18:25:16 +0800

    新建数组

--- kotlindemo/src/practice1.kt
+++ kotlindemo/src/practice1.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>) {
+    //联系数组
     val arr = Array(5, { i -> (i * i).toString() })
     for (s in arr) {
         println(s)
+    }
+    val arr2:IntArray = intArrayOf(1,2,3,4,5)
+    for (i in arr2) {
+        println(i)
     }
 }
